64ccfc05359566d022fd144ef1a398478dad8b51
Martin Shi <martinshi@Martins-MacBook-Pro.local>
2021-04-10 19:07:38 -0700

    graphs work + frequency shown

--- lifestyle.py
+++ lifestyle.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plot
 
 # all columns
 allCols = pd.read_csv("data/lifestyle.csv")
-allCols = allCols.iloc[:, 1:-3]
+allCols = allCols.iloc[:40, 1:-3]
 print(allCols.columns)
 # for s in allCols.columns:
 #     print(s)
@@ -14,19 +14,22 @@ models = []
 numCols = len(allCols.columns)
 for i in range(numCols):
     X = allCols.iloc[:, i]
+    X = X.values.reshape(-1, 1)
+    print(X)
     # X = allCols[1]
-    y = pd.read_csv("data/lifestyle.csv", usecols=[2])
+    y = pd.read_csv("data/lifestyle.csv", usecols=[2], nrows=40)
     y = y.astype('int')
 
     lm = linear_model.LinearRegression()
-    model = lm.fit(X.reshape(-1, 1), y)
+    model = lm.fit(X, y)
     models.append({
         'coef': lm.coef_,
         'intercept': lm.intercept_,
         'score': lm.score
     })
-    plot.scatter(X, y, color='red')
+    plot.scatter(X, y, color='red', alpha=.1)
     plot.plot(X, lm.predict(X), color='blue')
+    plot.xlabel(allCols.columns[i])
     plot.show()
 
 # sortedmodels = sorted(models, key=lambda k: abs(k['coef']))
